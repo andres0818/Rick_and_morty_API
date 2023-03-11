0293efd046e5c7dfe35942e4999d1485547cb897
andres0818 <c.andres.perez.h@gmail.com>
2023-03-11 00:00:37 -0500

    Add loading data

--- src/coponents/HomeScreen.js
+++ src/coponents/HomeScreen.js
@@ -1,22 +1,27 @@
 import React, {useContext} from 'react';
 import {View, FlatList} from 'react-native';
 import {cardConText} from '../Context/Context';
-import {styles} from '../styles/styles';
+import Loagind from '../Loading/Loading';
 import CardList from './CardList';
+import {styles} from '../styles/styles';
 
 export const HomeScreen = ({navigation}) => {
   const {data} = useContext(cardConText);
-
+  console.log(Loagind());
   return (
     <View style={styles.container}>
-      <FlatList
-        data={data}
-        numColumns={2}
-        columnWrapperStyle={styles.row}
-        renderItem={({item: card}) => (
-          <CardList {...card} navigation={navigation} />
-        )}
-      />
+      {data ? (
+        <FlatList
+          data={data}
+          numColumns={2}
+          columnWrapperStyle={styles.row}
+          renderItem={({item: card}) => (
+            <CardList {...card} navigation={navigation} />
+          )}
+        />
+      ) : (
+        <Loagind />
+      )}
     </View>
   );
 };
