c1276526adb6d8c107cb5a546a9b2cde139db7c9
andres0818 <c.andres.perez.h@gmail.com>
2023-04-03 16:27:01 -0500

    change styles for species

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -1,7 +1,13 @@
 import React, {useContext} from 'react';
-import {Button, Image, Text, View} from 'react-native';
+import {
+  Button,
+  Image,
+  Text,
+  TouchableWithoutFeedback,
+  View,
+} from 'react-native';
 import {dispatchCardText} from '../Context/Context';
-import {styles} from '../styles/styles';
+import {cardStyle} from '../styles/cards';
 
 const CardList = ({navigation, ...props}) => {
   const {setDetails} = useContext(dispatchCardText);
@@ -10,17 +16,26 @@ const CardList = ({navigation, ...props}) => {
     setDetails(data);
     return navigation.navigate('Details');
   };
+
+  const containerStyle = [
+    cardStyle.cardsContainer,
+    props.species === 'Human' && cardStyle.Human,
+    props.species === 'Alien' && cardStyle.Alien,
+    props.species === 'Mythological Creature' && cardStyle.Mytological,
+  ];
+
   return (
-    <View style={styles.cardsContainer} key={props.id}>
-      <Image style={styles.image} source={{uri: props.image}} />
-      <Text style={styles.text}>{props.name}</Text>
-      <Text style={styles.text}>{props.species}</Text>
-      <Button
-        title="Open"
-        color="#f194ff"
-        onPress={() => handlerPress(props)}
-      />
-    </View>
+    <TouchableWithoutFeedback onPress={() => handlerPress(props)}>
+      <View style={containerStyle} key={props.id}>
+        <View style={cardStyle.containerDescription}>
+          <Text style={cardStyle.title}>{props.name}</Text>
+          <Text style={cardStyle.text}>Specie: {props.species}</Text>
+        </View>
+        <View>
+          <Image style={cardStyle.image} source={{uri: props.image}} />
+        </View>
+      </View>
+    </TouchableWithoutFeedback>
   );
 };
 
