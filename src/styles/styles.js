c572909f6c5d2d6bba66a0819750ba6bf64538e4
andres0818 <c.andres.perez.h@gmail.com>
2023-04-03 16:26:40 -0500

    change styles and clear style

--- src/styles/styles.js
+++ src/styles/styles.js
@@ -3,7 +3,6 @@ import {StyleSheet} from 'react-native';
 export const styles = StyleSheet.create({
   container: {
     flex: 1,
-    width: '100%',
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
@@ -14,45 +13,17 @@ export const styles = StyleSheet.create({
     borderRadius: 19,
     marginVertical: 10,
     paddingHorizontal: 12,
+    color: 'black',
   },
   row: {
-    flex: 1,
-    justifyContent: 'space-evenly',
+    width: 380,
+    marginHorizontal: '5%',
+    padding: 15,
   },
   titulo: {
     fontSize: 30,
     textAlign: 'center',
     margin: 10,
     fontWeight: 'bold',
-  },
-  image: {
-    width: 140,
-    height: 140,
-    resizeMode: 'cover',
-    borderRadius: 8,
-  },
-  cardsContainer: {
-    margin: 5,
-    borderRadius: 8,
-    padding: 5,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#E0E0E0',
-    width: '43%',
-    elevation: 15,
-  },
-  text: {
-    color: 'black',
-    fontWeight: 'bold',
-    textAlign: 'center',
-  },
-  sectionContainer: {
-    flex: 1,
-    width: '100%',
-    height: '100%',
-    flexDirection: 'row',
-    flexWrap: 'wrap',
-    justifyContent: 'center',
-    marginBottom: '20%',
   },
 });
