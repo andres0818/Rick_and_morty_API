d35594e889ac985f960c0b906ff8b15c715b9790
andres0818 <c.andres.perez.h@gmail.com>
2023-04-03 16:27:28 -0500

    create styles for component Cards

---
+++ src/styles/cards.js
@@ -0,0 +1,46 @@
+import {StyleSheet} from 'react-native';
+import {blue, green, purple} from './color';
+
+export const cardStyle = StyleSheet.create({
+  Human: {
+    backgroundColor: blue,
+  },
+  Alien: {
+    backgroundColor: green,
+  },
+  Mytological: {
+    backgroundColor: purple,
+  },
+  cardsContainer: {
+    flexDirection: 'row',
+    marginVertical: 12,
+    borderRadius: 8,
+    padding: 8,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#E0E0E0',
+    width: '90%',
+    elevation: 10,
+  },
+  containerDescription: {
+    width: '50%',
+    padding: 5,
+  },
+  text: {
+    color: 'black',
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  title: {
+    color: 'black',
+    fontWeight: 'bold',
+    textAlign: 'center',
+    fontSize: 18,
+  },
+  image: {
+    width: 140,
+    height: 140,
+    resizeMode: 'cover',
+    borderRadius: 8,
+  },
+});
